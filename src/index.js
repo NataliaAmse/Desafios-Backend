76405a90d8eaa44d23c85e773707f5e6d58698ac
Natalia Amse <natalia.amse@gmail.com>
2024-03-09 16:26:16 -0300

    last changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import cartRouter from './routes/cartRouter.js'
 import productsRouter from './routes/productsRouter.js'
+import userRouter from './routes/userRouter.js'
+import chatRouter from './routes/chatRouter.js'
 import upload from './config/multer.js'
+import mongoose from 'mongoose'
+import messageModel from './models/messages.js'
 import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
 import { __dirname } from './path.js'
@@ -17,6 +21,11 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server)
 
+//Connection DB
+mongoose.connect("mongodb+srv://nataliaamse:Berlin2024@cluster0.ayfaxwz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("DB is connected"))
+    .catch(e => console.log(e))
+
 //Middlewares
 app.use(express.json())
 app.engine('handlebars', engine())
@@ -26,23 +35,25 @@ app.set('views', __dirname + '/views')
 io.on('connection', (socket) => {
     console.log("Conexion con Socket.io")
 
-    socket.on('movimiento', info => { //Cuando el cliente me envia un mensaje, lo capturo y lo muestro
-        console.log(info)
-    })
+    socket.on('mensaje', async (mensaje) => {
+        try {
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        } catch (e) {
+            io.emit('mensajeLogs', e)
+        }
 
-    socket.on('rendirse', info => { //Cuando el cliente me envia un mensaje, lo capturo y lo muestro
-        console.log(info)
-        socket.emit('mensaje-jugador', "Te has rendido") //Cliente que envio este mensaje
-        socket.broadcast.emit('rendicion', "El jugador se rindio") //Clientes que tengan establecida la comunicacion con el servidor
     })
 
 })
 
-
 //Routes
-app.use('/static', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
 app.use('/api/products', productsRouter, express.static(__dirname + '/public'))
 app.use('/api/cart', cartRouter)
+app.use('/api/chat', chatRouter, express.static(__dirname + '/public'))
+app.use('/api/users', userRouter)
 
 app.post('/upload', upload.single('product'), (req, res) => {
     try {
@@ -52,20 +63,3 @@ app.post('/upload', upload.single('product'), (req, res) => {
         res.status(500).send("Error al cargar imagen")
     }
 })
-
-/*
-app.get('/static', (req, res) => {
-
-    const prods = [
-        { id: 1, title: "Celular", price: 1500, img: "./img/170718249838066585_7797470128152.jpg" },
-        { id: 2, title: "Televisor", price: 1800, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" },
-        { id: 3, title: "Tablet", price: 1200, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" },
-        { id: 4, title: "Notebook", price: 1900, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" }
-    ]
-
-    res.render('templates/products', {
-        mostrarProductos: true,
-        productos: prods,
-        css: 'product.css'
-    })
-})*/
