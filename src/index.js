0d23dd5bf45c859c4ecf51a88d7f31b203ae5ad1
Natalia Amse <natalia.amse@gmail.com>
2024-03-29 12:47:17 -0300

    utimos cambios

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
+import passport from 'passport'
 import cookieParser from 'cookie-parser'
 import messageModel from './models/messages.js'
 import indexRouter from './routes/indexRouter.js'
+import initializePassport from './config/passport/passport.js'
 import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
 import { __dirname } from './path.js'
@@ -12,6 +14,7 @@ import { __dirname } from './path.js'
 //Configuraciones o declaraciones
 const app = express()
 const PORT = 8000
+
 
 //Server
 const server = app.listen(PORT, () => {
@@ -29,9 +32,9 @@ mongoose.connect("mongodb+srv://nataliaamse:Berlin2024@cluster0.ayfaxwz.mongodb.
 app.use(express.json())
 
 app.use(session({
-secret: "coderSecret",
-resave: true,
-store: MongoStore.create({
+    secret: "coderSecret",
+    resave: true,
+    store: MongoStore.create({
     mongoUrl: "mongodb+srv://nataliaamse:Berlin2024@cluster0.ayfaxwz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     ttl: 60 * 60
 }),
@@ -42,60 +45,66 @@ app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views')
 
+initializePassport()
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Routes
+
 app.use('/', indexRouter)
 
 //Routes Cookies
 app.get('/setCookie', (req, res) => {
-res.cookie('CookieCookie', 'Esto es una cookie :)', { maxAge: 3000000, signed: true }).send("Cookie creada")
+    res.cookie('CookieCookie', 'Esto es una cookie :)', { maxAge: 3000000, signed: true }).send("Cookie creada")
 })
 
 app.get('/getCookie', (req, res) => {
-res.send(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.get('/deleteCookie', (req, res) => {
-res.clearCookie('CookieCookie').send("Cookie eliminada")
-//res.cookie('CookieCokie', '', { expires: new Date(0) })
+    res.clearCookie('CookieCookie').send("Cookie eliminada")
+    //res.cookie('CookieCokie', '', { expires: new Date(0) })
 })
 
 //Session Routes
 
 app.get('/session', (req, res) => {
-console.log(req.session)
-if (req.session.counter) {
-    req.session.counter++
-    res.send(`Sos el usuario N° ${req.session.counter} en ingresar a la pagina`)
-} else {
-    req.session.counter = 1
-    res.send("Sos el primer usuario que ingresa a la pagina")
-}
+    console.log(req.session)
+    if (req.session.counter) {
+        req.session.counter++
+        res.send(`Sos el usuario N° ${req.session.counter} en ingresar a la pagina`)
+    } else {
+        req.session.counter = 1
+        res.send("Sos el primer usuario que ingresa a la pagina")
+    }
 })
 
 app.post('/login', (req, res) => {
-const { email, password } = req.body
+    const { email, password } = req.body
 
-if (email == "admin@admin.com" && password == "1234") {
-    req.session.email = email
-    req.session.password = password
+    if (email == "admin@admin.com" && password == "1234") {
+        req.session.email = email
+        req.session.password = password
 
 
-}
-console.log(req.session)
-res.send("Login")
+    }
+    console.log(req.session)
+    res.send("Login")
 })
 
 io.on('connection', (socket) => {
-console.log("Conexion con Socket.io")
+    console.log("Conexion con Socket.io")
 
-socket.on('mensaje', async (mensaje) => {
-    try {
-        await messageModel.create(mensaje)
-        const mensajes = await messageModel.find()
-        io.emit('mensajeLogs', mensajes)
-    } catch (e) {
-        io.emit('mensajeLogs', e)
-    }
+    socket.on('mensaje', async (mensaje) => {
+        try {
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        } catch (e) {
+            io.emit('mensajeLogs', e)
+        }
 
-})
+    })
 
 })
