ff5455b558fc37ee505b3b83a52b346231d03cbb
Natalia Amse <natalia.amse@gmail.com>
2024-06-02 13:23:22 -0300

    ultimos cambios

--- Backend/src/controllers/sessionController.js
+++ Backend/src/controllers/sessionController.js
@@ -1,4 +1,7 @@
-import passport from "passport";
+import { userModel } from "../models/user.js";
+import { sendEmailChangePassword } from "../utils/nodemailer.js";
+import jwt from 'jsonwebtoken';
+import { validatePassword, createHash } from "../utils/bcrypt.js";
 
 export const login = async (req, res) => {
     try {
@@ -60,4 +63,58 @@ export const testJWT = async (req, res) => {
         res.status(403).send("Usuario no autorizado")
     else
         res.status(200).send(req.user)
+}
+
+export const changePassword = async(req,res) => {
+    const {token} = req.params
+    const {newPassword} = req.body
+
+
+    try {
+        const validateToken = jwt.verify(token.substr(6,), "coder")
+        const user = await userModel.findOne({ email: validateToken.userEmail })
+        if(user) {
+            if(!validatePassword (newPassword, user.password)) {
+                const hashPassword = createHash(newPassword)
+                user.password = hashPassword
+                const resultado = await userModel.findByIdAndUpdate(user._id, user)
+                console.log(resultado)
+                res.status(200).send("Contraseña modificada correctamente")
+            }else {
+                res.status(400).send("La contraseña no puede ser identica a la anterior")
+                //contraseñas iguales
+
+            }
+        }else {
+            res.status(404).send("Usuario no encontrado")
+            //usuario no existe
+        }
+
+    }catch(e) {
+        console.log(e)
+        if(e?.message == 'jwt expired'){
+            res.status(400).send("Paso el tiempo máximo para recuperar la contraseña")
+        }
+       res.status(500).send(e)
+    }
+}
+
+export const sendEmailPassword = async(req,res) => {
+    try {
+        const {email} = req.body
+        const user = await userModel.find({ email : email})
+    
+        if (user) {
+            const token = jwt.sign({userEmail: email}, "coder", { expiresIn: '1h' })
+            const resetLink = `http://localhost:8000/api/session/reset-password?token=${token}`
+            sendEmailChangePassword(email, resetLink)
+            res.status(200).send("Email enviado correctamente")
+        } else {
+            res.status(404).send("Usuario no encontrado")
+        }    
+
+    }catch(e) {
+        res.status(500).send(e)
+    }
+    
 }
---
+++ Backend/src/utils/nodemailer.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport ({
+    service: 'gmail',
+    auth: {
+        user: "natalia.amse@gmail.com"
+        pass: ""
+    }
+})
+
+export const sendEmailChangePassword = async(email, linkChangePassword) => {
+    const mailOptions = {
+        from: "natalia.amse@gmail.com",
+        to: email,
+        subject: "Recuperacion de contraseña",
+        
+        html: 
+        `
+        <p>Haz click aqui para cambiar tu contraseña: </p> <button> <a href=${linkChangePassword}>Cambiar contraseña</a></button>
+        `
+    }
+
+    transporter.sendMail(mailOption, (error, info) => {
+        if(error) {
+            console.log("Error al enviar correo de cambio de contraseña")
+        } else {
+            console.log("Correo enviado correctamente", info.response)
+        }
+    })
+}
